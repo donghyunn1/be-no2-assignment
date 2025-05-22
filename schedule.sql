afae4ced0aac32897b1a6a48c4f57963ba6ad87e
양동현 <dongdong0516@naver.com>
2025-05-23 02:02:47 +0900

    sql 작성, db연결

---
+++ schedule.sql
@@ -0,0 +1,8 @@
+CREATE TABLE schedule (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    todo VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+);
